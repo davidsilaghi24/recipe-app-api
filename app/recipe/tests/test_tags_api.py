5dde1cb462613b1b307de745dc6fce25ceeb2a84
davidsilaghi24 <david.silaghi24@gmail.com>
2023-04-26 15:06:38 +0300

    Fixed typos

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -12,7 +12,7 @@ from core.models import Tag
 
 from recipe.serializers import TagSerializer
 
-TAGS_URL = reverse('reipe:tag-list')
+TAGS_URL = reverse('recipe:tag-list')
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return an user"""
@@ -66,4 +66,4 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
-        self.assertEqual(res.data[0]['id', tag.id])
+        self.assertEqual(res.data[0]['id'], tag.id)
